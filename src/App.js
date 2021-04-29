156f8487e398752435e72dcfda9e34b8f09b2d83
therealcy6 <48657171+therealcy6@users.noreply.github.com>
2021-04-29 10:25:49 -0400

    Firebase realtime working?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import CountDownTimer from './Timer.js';
+import React from 'react'
+import { Provider } from 'react-redux'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import { createStore, combineReducers } from 'redux'
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from 'react-redux-firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdfLtZMmiJBTY3BQ9WxftesK-wGxX-Ego",
+  authDomain: "cursed-sword-tracker.firebaseapp.com",
+  databaseURL: "https://cursed-sword-tracker-default-rtdb.firebaseio.com",
+  projectId: "cursed-sword-tracker",
+  storageBucket: "cursed-sword-tracker.appspot.com",
+  messagingSenderId: "769622462914",
+  appId: "1:769622462914:web:bce846260b796e092d065b",
+  measurementId: "G-2S7XN25YDS"
+};
+
+const rrfConfig = {
+  userProfile: 'users'
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer
+});
+
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+          <CountDownTimer></CountDownTimer>
+        </ReactReduxFirebaseProvider>
+      </Provider>
     </div>
   );
-}
+};
 
 export default App;
