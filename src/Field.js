5fd717fae30f4d9c9614c54a0e2804b45b02cac3
therealcy6 <48657171+therealcy6@users.noreply.github.com>
2021-05-01 18:29:25 -0400

    Less ugly.

--- src/Field.js
+++ src/Field.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import  'firebase/database'
+import './Field.css';
 import firebase from 'firebase/app'
 import CountDownTimer from './CountdownTimer.js'
 
@@ -36,20 +37,28 @@ class Field extends React.Component {
       
     render() {
         return (
-            <div>
-                <div>
-                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="360" name="Akamanah Chest" index="akachest"></CountDownTimer>
-                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Akamanah Priest 1" index="akapriest1"></CountDownTimer>
-                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Akamanah Priest 2" index="akapriest2"></CountDownTimer>
-                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Akamanah Priest 3" index="akapriest3"></CountDownTimer>
-                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Akamanah Priest 4" index="akapriest4"></CountDownTimer>
-                </div>
-                <div>
-                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="360" name="Zariche Chest" index="zarchest"></CountDownTimer>
+            <div className="field">
+                <div className="zariche">
                     <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Zariche Priest 1" index="zarpriest1"></CountDownTimer>
+                    <div></div>
                     <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Zariche Priest 2" index="zarpriest2"></CountDownTimer>
+                    <div></div>
+                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="360" name="Zariche Chest" index="zarchest"></CountDownTimer>
+                    <div></div>
                     <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Zariche Priest 3" index="zarpriest3"></CountDownTimer>
+                    <div></div>
                     <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Zariche Priest 4" index="zarpriest4"></CountDownTimer>
+                </div>
+                <div className="akamanah">                    
+                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Akamanah Priest 1" index="akapriest1"></CountDownTimer>
+                    <div></div>
+                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Akamanah Priest 2" index="akapriest2"></CountDownTimer>
+                    <div></div>
+                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="360" name="Akamanah Chest" index="akachest"></CountDownTimer>
+                    <div></div>
+                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Akamanah Priest 3" index="akapriest3"></CountDownTimer>
+                    <div></div>
+                    <CountDownTimer now={this.state.now} kill={this.kill} respawn="480" name="Akamanah Priest 4" index="akapriest4"></CountDownTimer>
                 </div>
             </div>
         )
